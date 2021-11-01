a4067aa8565ce6675ebf5ba23a310d6841940032
Park <rppr001@naver.com>
2021-11-01 22:23:25 +0900

    react-node-connect concurrently and node express-generator and proxy commit

---
+++ server.js
@@ -0,0 +1,14 @@
+
+var express = require('express');
+
+var indexRouter = require('./routes/index');
+var usersRouter = require('./routes/users');
+
+var app = express();
+
+
+app.use('/', indexRouter);
+app.use('/users', usersRouter);
+
+const port = process.env.PORT || 5000;
+app.listen(port, () => console.log(`Listenning on port ${port}`));
